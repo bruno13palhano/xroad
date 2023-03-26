af7b4d6d5af77eb33ff20f2356ba87c2531730bf
bruno13palhano <bruno13palhano@gmail.com>
2023-03-26 20:07:29 -0300

    Added new model

---
+++ model/src/main/java/com/example/model/model/Difficulty.kt
@@ -0,0 +1,9 @@
+package com.example.model.model
+
+enum class Difficulty(difficulty: String) {
+    VERY_EASY("VERY_EASY"),
+    EASY("EASY"),
+    NORMAL("NORMAL"),
+    HARD("HARD"),
+    VERY_HARD("VERY_HARD")
+}

e6612076b3a9fa67edccfb9464d005013b9f3349
bruno13palhano <bruno13palhano@gmail.com>
2023-03-28 20:37:18 -0300

    Added a method to restore the path values

--- app/src/main/java/com/example/xroad/ui/path/viewmodel/PathViewModel.kt
+++ app/src/main/java/com/example/xroad/ui/path/viewmodel/PathViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.xroad.ui.path.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.core.di.SimplePathRep
 import com.example.core.repository.PathRepository
 import com.example.model.model.Path
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,5 +17,11 @@ class PathViewModel @Inject constructor(
 
     fun getPath(id: Long): Flow<Path> {
         return pathRep.getPathById(id)
+    }
+
+    fun updatePath(path: Path) {
+        viewModelScope.launch {
+            pathRep.updatePath(path)
+        }
     }
 }

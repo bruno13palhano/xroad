f1db5a7c977f7c9ac3415794935e741a15cc8ace
bruno13palhano <bruno13palhano@gmail.com>
2023-03-27 21:24:32 -0300

    Save values to database

--- app/src/main/java/com/example/xroad/ui/newpath/NewPathDifficultyFragment.kt
+++ app/src/main/java/com/example/xroad/ui/newpath/NewPathDifficultyFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.example.model.model.Difficulty
+import com.example.model.model.Path
 import com.example.xroad.R
 import com.example.xroad.databinding.FragmentNewPathDifficultyBinding
+import com.example.xroad.ui.newpath.viewmodel.NewPathViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class NewPathDifficultyFragment : Fragment() {
     private var _binding: FragmentNewPathDifficultyBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: NewPathViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +29,81 @@ class NewPathDifficultyFragment : Fragment() {
         _binding = FragmentNewPathDifficultyBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        var title = ""
+        var topic = ""
+        var description = ""
+        var duration = 0L
+        var date = 0L
+        var difficulty = Difficulty.NORMAL
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            launch {
+                viewModel.title.collect {
+                    title = it
+                }
+            }
+            launch {
+                viewModel.topic.collect {
+                    topic = it
+                }
+            }
+            launch {
+                viewModel.description.collect {
+                    description = it
+                }
+            }
+            launch {
+                viewModel.duration.collect {
+                    duration = it
+                }
+            }
+            launch {
+                viewModel.date.collect {
+                    date = it
+                }
+            }
+            launch {
+                viewModel.difficulty.collect {
+                    difficulty = it
+                }
+            }
+        }
+
+        binding.difficultyGroup.setOnCheckedChangeListener { radioGroup, i ->
+            when (radioGroup.checkedRadioButtonId) {
+                binding.veryEasyDifficulty.id -> {
+                    viewModel.setDifficulty(Difficulty.VERY_EASY)
+                }
+                binding.easyDifficulty.id -> {
+                    viewModel.setDifficulty(Difficulty.EASY)
+                }
+                binding.normalDifficulty.id -> {
+                    viewModel.setDifficulty(Difficulty.NORMAL)
+                }
+                binding.hardDifficulty.id -> {
+                    viewModel.setDifficulty(Difficulty.HARD)
+                }
+                binding.veryHardDifficulty.id -> {
+                    viewModel.setDifficulty(Difficulty.VERY_HARD)
+                }
+            }
+        }
+
         binding.button.setOnClickListener {
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewModel.insertPath(
+                    Path(
+                        id = 0L,
+                        title = title,
+                        topic = topic,
+                        description = description,
+                        durationInMilliseconds = duration,
+                        dateInMilliseconds = date,
+                        difficulty = difficulty
+                    )
+                )
+            }
+
             findNavController().navigate(
                 NewPathDifficultyFragmentDirections.actionDifficultyToTitleAndTopic())
         }

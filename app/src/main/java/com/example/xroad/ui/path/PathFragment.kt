03c4e84c3692ecd93bdf96d3823aaa0a2063a8d9
bruno13palhano <bruno13palhano@gmail.com>
2023-03-27 21:21:27 -0300

    Added bindingView and PathViewModel

--- app/src/main/java/com/example/xroad/ui/path/PathFragment.kt
+++ app/src/main/java/com/example/xroad/ui/path/PathFragment.kt
@@ -5,23 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.xroad.R
+import com.example.xroad.databinding.FragmentPathBinding
+import com.example.xroad.ui.path.viewmodel.PathViewModel
 import com.google.android.material.appbar.MaterialToolbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class PathFragment : Fragment() {
+    private var _binding: FragmentPathBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: PathViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_path, container, false)
+    ): View {
+        _binding = FragmentPathBinding.inflate(inflater, container, false)
+        val view = binding.root
 
         val roadId = PathFragmentArgs.fromBundle(requireArguments()).roadId
 
-        val productId = view.findViewById<TextView>(R.id.road_id)
-        productId.text = "road id: $roadId"
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.getPath(roadId).collect {
+                    binding.pathTitle.text = it.title
+                    binding.pathTopic.text = it.topic
+                    binding.pathDescription.text = it.description
+                    binding.pathDuration.text = it.durationInMilliseconds.toString()
+                    binding.pathDate.text = it.dateInMilliseconds.toString()
+                }
+            }
+        }
+
+        binding.doneButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
 
         return view
     }
@@ -30,7 +55,7 @@ class PathFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         toolbar.setNavigationIcon(R.drawable.baseline_arrow_back_24)
-        toolbar.title = "Road"
+        toolbar.title = getString(R.string.path_label)
 
         toolbar.setNavigationOnClickListener {
             findNavController().navigateUp()

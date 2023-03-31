46ac6f83f6c804f7e70a97df697c5c883b502c27
bruno13palhano <bruno13palhano@gmail.com>
2023-03-31 15:21:13 -0300

    Created a viewModel

--- app/src/main/java/com/example/xroad/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/xroad/ui/home/HomeViewModel.kt
@@ -1,4 +1,35 @@
 package com.example.xroad.ui.home
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.core.di.SimplePathRep
+import com.example.core.repository.PathRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    @SimplePathRep private val pathRepository: PathRepository
+) : ViewModel() {
+
+    val uiState = combine(
+        pathRepository.getPathCountStream(),
+        pathRepository.getLastPathStream()
+    ) { days, lastPath ->
+        HomeUiState(
+            days = days,
+            pathTitle = lastPath.title,
+            pathTopic = lastPath.topic,
+            pathDuration = lastPath.durationInMilliseconds,
+            pathDate = lastPath.dateInMilliseconds
+        )
+    }
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = HomeUiState(),
+            started = WhileSubscribed(5_000)
+        )
 }

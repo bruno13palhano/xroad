9e87718b1aa4833b4069511736335ed2442c906c
bruno13palhano <bruno13palhano@gmail.com>
2023-04-03 15:24:54 -0300

    Adde new enum classes

---
+++ model/src/main/java/com/example/model/model/Day.kt
@@ -0,0 +1,11 @@
+package com.example.model.model
+
+enum class Day(val day: String) {
+    SUNDAY("SUNDAY"),
+    MONDAY("MONDAY"),
+    TUESDAY("TUESDAY"),
+    WEDNESDAY("WEDNESDAY"),
+    THURSDAY("THURSDAY"),
+    FRIDAY("FRIDAY"),
+    SATURDAY("SATURDAY")
+}
---
+++ model/src/main/java/com/example/model/model/Month.kt
@@ -0,0 +1,16 @@
+package com.example.model.model
+
+enum class Month(val month: String) {
+    JANUARY("JANUARY"),
+    FEBRUARY("FEBRUARY"),
+    MARCH("MARCH"),
+    APRIL("APRIL"),
+    MAY("MAY"),
+    JUNE("JUNE"),
+    JULY("JULY"),
+    AUGUST("AUGUST"),
+    SEPTEMBER("SEPTEMBER"),
+    OCTOBER("OCTOBER"),
+    NOVEMBER("NOVEMBER"),
+    DECEMBER("DECEMBER")
+}

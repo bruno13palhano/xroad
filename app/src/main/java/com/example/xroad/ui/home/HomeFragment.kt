013b5e74b017e8eb93a6117db53088066d09e6ca
bruno13palhano <bruno13palhano@gmail.com>
2023-04-06 21:04:34 -0300

    Changed viewBinding to dataBinding

--- app/src/main/java/com/example/xroad/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/xroad/ui/home/HomeFragment.kt
@@ -5,16 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.xroad.R
 import com.example.xroad.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -26,24 +23,12 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         val view = binding.root
 
-        binding.addButton.setOnClickListener {
-            findNavController().navigate(
-                HomeFragmentDirections.actionHomeToNewPath()
-            )
-        }
-
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    viewModel.uiState.collect {
-                        setUiState(it)
-                    }
-                }
-            }
-        }
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        binding.events = this
 
         return view
     }
@@ -53,21 +38,15 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    private fun setUiState(uiState: HomeUiState) {
-        binding.days.text = getString(R.string.days_label, uiState.days)
-        binding.lastPathTitle.text = getString(R.string.last_path_title_label, uiState.pathTitle)
-        binding.lastPathTopic.text = getString(R.string.last_path_topic_label, uiState.pathTopic)
-        binding.lastPathDuration.text = getString(R.string.last_path_duration_label,
-            durationInMillisecondsToString(uiState.pathDuration))
-        binding.lastPathDate.text = getString(R.string.last_path_date_label,
-            dateInMillisecondsToString(uiState.pathDate))
-    }
-
-    private fun durationInMillisecondsToString(duration: Long): String {
+    fun durationInMillisecondsToString(duration: Long): String {
         return DateFormat.getPatternInstance(DateFormat.HOUR24_MINUTE).format(duration)
     }
 
-    private fun dateInMillisecondsToString(date: Long): String {
+    fun dateInMillisecondsToString(date: Long): String {
         return DateFormat.getPatternInstance(DateFormat.YEAR_ABBR_MONTH_WEEKDAY_DAY).format(date)
+    }
+
+    fun navigateToNewPath() {
+        findNavController().navigate(HomeFragmentDirections.actionHomeToNewPath())
     }
 }

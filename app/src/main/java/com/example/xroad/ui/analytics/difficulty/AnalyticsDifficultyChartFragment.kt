71c328c4c6c51549b56ee2e27092d03a6dbace12
bruno13palhano <bruno13palhano@gmail.com>
2023-04-16 16:17:29 -0300

    Refactored

--- app/src/main/java/com/example/xroad/ui/analytics/difficulty/AnalyticsDifficultyChartFragment.kt
+++ app/src/main/java/com/example/xroad/ui/analytics/difficulty/AnalyticsDifficultyChartFragment.kt
@@ -17,6 +17,7 @@ import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAStyle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -33,44 +34,52 @@ class AnalyticsDifficultyChartFragment : Fragment() {
             .inflate(inflater, R.layout.fragment_analytics_difficulty_chart, container, false)
         val view = binding.root
 
+        val chartModel: AAChartModel = AAChartModel()
+            .chartType(AAChartType.Column)
+            .title(getString(R.string.hours_difficulty_label))
+            .titleStyle(AAStyle().color(getString(R.string.white)))
+            .subtitle(getString(R.string.hour_difficulty_label))
+            .subtitleStyle(AAStyle().color(getString(R.string.white)))
+            .dataLabelsEnabled(true)
+            .backgroundColor(getString(R.string.gray_primary))
+            .axesTextColor(getString(R.string.white))
+            .dataLabelsStyle(AAStyle().color(getString(R.string.white)))
+            .series(
+                arrayOf(
+                    AASeriesElement()
+                        .name(getString(R.string.average_hours_label))
+                        .data(arrayOf(0.0F, 0.0F, 0.0F, 0.0F, 0.0F))
+                        .color(getString(R.string.july_yellow_label))
+                )).categories(
+                arrayOf(
+                    getString(R.string.very_easy_label),
+                    getString(R.string.easy_label),
+                    getString(R.string.normal_label),
+                    getString(R.string.hard_label),
+                    getString(R.string.very_hard_label)
+                )
+            )
+
+        binding.difficultyChart.aa_drawChartWithChartModel(chartModel)
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.uiState.collect {
-                    val chartModel: AAChartModel = AAChartModel()
-                        .chartType(AAChartType.Column)
-                        .title(getString(R.string.hours_difficulty_label))
-                        .titleStyle(AAStyle().color(getString(R.string.white)))
-                        .subtitle(getString(R.string.hour_difficulty_label))
-                        .subtitleStyle(AAStyle().color(getString(R.string.white)))
-                        .dataLabelsEnabled(true)
-                        .backgroundColor(getString(R.string.gray_primary))
-                        .axesTextColor(getString(R.string.white))
-                        .dataLabelsStyle(AAStyle().color(getString(R.string.white)))
-                        .series(
-                            arrayOf(
-                                AASeriesElement()
-                                    .name(getString(R.string.average_hours_label))
-                                    .data(
-                                        arrayOf(
-                                            it.veryEasyAverageDuration,
-                                            it.easyAverageDuration,
-                                            it.normalAverageDuration,
-                                            it.hardAverageDuration,
-                                            it.veryHardAverageDuration
-                                        )
+                    delay(200)
+                    binding.difficultyChart.aa_onlyRefreshTheChartDataWithChartOptionsSeriesArray(
+                        arrayOf(
+                            AASeriesElement()
+                                .data(
+                                    arrayOf(
+                                        it.veryEasyAverageDuration,
+                                        it.easyAverageDuration,
+                                        it.normalAverageDuration,
+                                        it.hardAverageDuration,
+                                        it.veryHardAverageDuration
                                     )
-                                    .color(getString(R.string.july_yellow_label))
-                            )).categories(
-                            arrayOf(
-                                getString(R.string.very_easy_label),
-                                getString(R.string.easy_label),
-                                getString(R.string.normal_label),
-                                getString(R.string.hard_label),
-                                getString(R.string.very_hard_label)
-                            )
+                                )
                         )
-
-                    binding.difficultyChart.aa_drawChartWithChartModel(chartModel)
+                    )
                 }
             }
         }
--- app/src/main/java/com/example/xroad/ui/analytics/week/AnalyticsWeekChartFragment.kt
+++ app/src/main/java/com/example/xroad/ui/analytics/week/AnalyticsWeekChartFragment.kt
@@ -17,6 +17,7 @@ import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAStyle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -33,48 +34,56 @@ class AnalyticsWeekChartFragment : Fragment() {
             .inflate(inflater, R.layout.fragment_analytics_week_chart, container, false)
         val view = binding.root
 
+        val chartModel: AAChartModel = AAChartModel()
+            .chartType(AAChartType.Column)
+            .title(getString(R.string.hours_day_week_label))
+            .titleStyle(AAStyle().color(getString(R.string.white)))
+            .subtitle(getString(R.string.average_hours_week_day_label))
+            .subtitleStyle(AAStyle().color(getString(R.string.white)))
+            .dataLabelsEnabled(true)
+            .backgroundColor(getString(R.string.gray_primary))
+            .axesTextColor(getString(R.string.white))
+            .dataLabelsStyle(AAStyle().color(getString(R.string.white)))
+            .series(
+                arrayOf(
+                    AASeriesElement()
+                        .name(getString(R.string.average_hours_label))
+                        .data(arrayOf(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F))
+                        .color(getString(R.string.july_yellow_label)),
+                )).categories(
+                arrayOf(
+                    getString(R.string.sunday_label),
+                    getString(R.string.monday_label),
+                    getString(R.string.tuesday_label),
+                    getString(R.string.wednesday_label),
+                    getString(R.string.thursday_label),
+                    getString(R.string.friday_label),
+                    getString(R.string.saturday_label)
+                )
+            )
+
+        binding.weekChart.aa_drawChartWithChartModel(chartModel)
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.uiState.collect {
-                    val chartModel: AAChartModel = AAChartModel()
-                        .chartType(AAChartType.Column)
-                        .title(getString(R.string.hours_day_week_label))
-                        .titleStyle(AAStyle().color(getString(R.string.white)))
-                        .subtitle(getString(R.string.average_hours_week_day_label))
-                        .subtitleStyle(AAStyle().color(getString(R.string.white)))
-                        .dataLabelsEnabled(true)
-                        .backgroundColor(getString(R.string.gray_primary))
-                        .axesTextColor(getString(R.string.white))
-                        .dataLabelsStyle(AAStyle().color(getString(R.string.white)))
-                        .series(
-                            arrayOf(
-                                AASeriesElement()
-                                    .name(getString(R.string.average_hours_label))
-                                    .data(
-                                        arrayOf(
-                                            it.sunday,
-                                            it.monday,
-                                            it.tuesday,
-                                            it.wednesday,
-                                            it.thursday,
-                                            it.friday,
-                                            it.saturday
-                                        )
+                    delay(200)
+                    binding.weekChart.aa_onlyRefreshTheChartDataWithChartOptionsSeriesArray(
+                        arrayOf(
+                            AASeriesElement()
+                                .data(
+                                    arrayOf(
+                                        it.sunday,
+                                        it.monday,
+                                        it.tuesday,
+                                        it.wednesday,
+                                        it.thursday,
+                                        it.friday,
+                                        it.saturday
                                     )
-                                    .color(getString(R.string.july_yellow_label)),
-                            )).categories(
-                                arrayOf(
-                                    getString(R.string.sunday_label),
-                                    getString(R.string.monday_label),
-                                    getString(R.string.tuesday_label),
-                                    getString(R.string.wednesday_label),
-                                    getString(R.string.thursday_label),
-                                    getString(R.string.friday_label),
-                                    getString(R.string.saturday_label)
                                 )
-                            )
-
-                    binding.weekChart.aa_drawChartWithChartModel(chartModel)
+                        )
+                    )
                 }
             }
         }
